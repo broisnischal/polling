4ce1e8439d4768960464b7a03384fc5a068624d8
broisnischal <neeswebservice@gmail.com>
2025-02-06 21:24:46 +0545

    feat:impl

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -28,15 +28,10 @@ export async function loader({ request }: Route.LoaderArgs) {
     };
   }
 
-  await db
-    .update(session)
-    .set({ status: "authorized" })
-    .where(eq(session.id, res.id));
-
   if (res?.status === "authorized") {
     return {
       token,
-      success: true,
+      authorized: true,
     };
   }
 
@@ -46,45 +41,72 @@ export async function loader({ request }: Route.LoaderArgs) {
   };
 }
 
-export async function action({ request }: Route.ActionArgs) { }
+export async function action({ request }: Route.ActionArgs) {
+  console.log("action")
 
-export default function login({ loaderData }: Route.ComponentProps) {
+  const formData = await request.formData();
+  const token = formData.get("token");
+
+  console.log(token)
+
+  const res = await db.query.session.findFirst({
+    where: (session, { eq }) => eq(session.token, token as string),
+  });
+
+  if (!res) {
+    return {
+      token,
+      authorized: false,
+    };
+  }
+  await db
+    .update(session)
+    .set({ status: "authorized" })
+    .where(eq(session.id, res.id));
+
+  return {
+    token,
+    authorized: true,
+  };
+}
+
+export default function login({ loaderData, actionData }: Route.ComponentProps) {
+  console.log(actionData);
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Login</h1>
 
       <br />
 
-      {loaderData.success && (
-        <div>
-          <p className="w-1/2">
-            Polling | This is a simple app demonstrating a short, long, and
-            immediate polling for the remote auth, using the cli, desktop, or
-            whatever.
-          </p>
-        </div>
-      )}
+      <div>
+        <p className="w-1/2">
+          Polling | This is a simple app demonstrating a short, long, and
+          immediate polling for the remote auth, using the cli, desktop, or
+          whatever.
+        </p>
+      </div>
       <br />
       {loaderData.token ? (
-        <Form className="flex flex-col gap-2 w-1/2 ">
-          {/* <input type="hidden" name="token"  /> */}
+        <Form method="POST" className="flex flex-col gap-2 w-1/2 ">
           <input
-            type="email"
-            className="border p-2 border-gray-800/10 focus:border-gray-800 outline-none"
-            placeholder="Enter your test email"
-          />
-          <input
-            type="token"
+            type="text"
+            name="token"
+            hidden
+            defaultValue={loaderData.token}
             className="border p-2 border-gray-800/10 focus:border-gray-800 outline-none"
             placeholder="Enter your test token"
           />
-          <button className="bg-gray-800 text-white p-2 w-min px-10">
+          <button
+            type="submit"
+            className="bg-gray-800 cursor-pointer text-white p-2 w-min px-10"
+          >
             Login
           </button>
         </Form>
       ) : (
         <div>
-          <p className="text-red-500 font-bold">
+          <p className="text-cyan-500 font-bold">
             Please try to login using the cli.
           </p>
           <p>
@@ -93,10 +115,17 @@ export default function login({ loaderData }: Route.ComponentProps) {
           </p>
         </div>
       )}
-      {/* <div>
-        <h1>Authentication Successful!</h1>
-        <p>You can now return to your CLI tool</p>
-      </div> */}
+
+      {loaderData.authorized ? (
+        <div>
+          <p>You are now logged in. and can close the tab.</p>
+        </div>
+      ) : (
+        <div>
+          <br />
+          <p>You are not authorized, please try again.</p>
+        </div>
+      )}
     </div>
   );
 }
