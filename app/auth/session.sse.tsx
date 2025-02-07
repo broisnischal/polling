a3654f9b7e5b04526825595d6e85a0bba0d15719
broisnischal <info@nischal-dahal.com.np>
2025-02-07 12:55:49 +0545

    feat:publ

--- app/auth/session.sse.tsx
+++ app/auth/session.sse.tsx
@@ -1,5 +1,8 @@
-import { eventStream } from "remix-utils";
+import { eventStream } from "remix-utils/sse/server";
+// import { interval } from "remix-utils/timers";
 import type { Route } from "./+types/session.sse";
+import { useEventSource } from "remix-utils/sse/react";
+
 // import { timeout } from "remix-utils/promise";
 // import { CSRF } from "remix-utils/csrf/server";
 
@@ -13,4 +16,21 @@ export async function loader({ request }: Route.LoaderArgs) {
       clearInterval(timer);
     };
   });
+}
+
+export default function Counter() {
+  // Here `/sse/time` is the resource route returning an eventStream response
+  let time = useEventSource("/session/sse", { event: "time" });
+
+  if (!time) return null;
+
+  return (
+    <time dateTime={time}>
+      {new Date(time).toLocaleTimeString("en", {
+        minute: "2-digit",
+        second: "2-digit",
+        hour: "2-digit",
+      })}
+    </time>
+  );
 }
